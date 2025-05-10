7aa0be262add7319c7f907c445ad4e7da8a8c27e
ChisaeHwang <cwh73090@naver.com>
2025-05-10 19:35:05 +0900

    refact: ElectronSessioNAdapter랑 BrowserCaptureSerivce 사이에 코드 중복 해결

--- src/features/calendar/services/index.ts
+++ src/features/calendar/services/index.ts
@@ -4,11 +4,23 @@
  * 의존성 관리와 역전을 위한 단일 진입점
  */
 
-// 서비스 내보내기
-export { sessionStorageService } from "./SessionStorageService";
-export { timerService } from "./TimerService";
-export { sessionManager } from "./SessionManager";
-export { DateService } from "./DateService";
+// 서비스 구현체들
+export * from "./DateService";
+export * from "./TimerService";
+export * from "./SessionManager";
+export * from "./SessionStorageService";
+export * from "./ElectronSessionAdapter";
+export * from "./BrowserCaptureService";
+
+// 새로 추가된 인터페이스와 클래스들
+export * from "./ICaptureService";
+export * from "./BaseCaptureService";
+export * from "./CaptureServiceFactory";
+export * from "./adapters/BrowserCaptureAdapter";
+export * from "./adapters/ElectronCaptureAdapter";
+
+// 서비스 인터페이스들
+export * from "./ServiceFactory";
 
 // 타입 내보내기
 export type { SessionState } from "./TimerService";
